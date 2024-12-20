6a9ce86653dd0956c030b62cd89c026094df816a
Emmanouil Athanasopoulos <70025614+athanasso@users.noreply.github.com>
2024-12-20 13:31:10 +0200

    Navigation for new components

--- src/app/app-navigation.ts
+++ src/app/app-navigation.ts
@@ -2,7 +2,22 @@ export const navigation = [
   {
     text: 'Home',
     path: '/home',
-    icon: 'home'
+    icon: 'home',
+  },
+  {
+    text: 'Overview',
+    path: '/overview',
+    icon: 'bookmark',
+  },
+  {
+    text: 'Country Reports',
+    path: '/country-reports',
+    icon: 'bookmark',
+  },
+  {
+    text: 'Weekly Chart',
+    path: '/weekly-chart',
+    icon: 'bookmark',
   },
   {
     text: 'Examples',
@@ -10,12 +25,12 @@ export const navigation = [
     items: [
       {
         text: 'Profile',
-        path: '/profile'
+        path: '/profile',
       },
       {
         text: 'Tasks',
-        path: '/tasks'
-      }
-    ]
-  }
+        path: '/tasks',
+      },
+    ],
+  },
 ];
